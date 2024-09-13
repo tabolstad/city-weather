1179edf25d326cff900288572c7ec91cfd7fd8ca
Tim Bolstad <tim@skilpaddelabs.com>
2024-09-13 16:21:23 -0400

    Initial Commit

---
+++ CityWeather/CityWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityWeatherApp.swift
+//  CityWeather
+//
+//  Created by Timothy Bolstad on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct CityWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CityWeatherTests/CityWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  CityWeatherTests.swift
+//  CityWeatherTests
+//
+//  Created by Timothy Bolstad on 9/13/24.
+//
+
+import Testing
+@testable import CityWeather
+
+struct CityWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
