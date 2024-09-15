aa34dcd1621cb3b9b4ab03a0c7a73348c90f869f
Tim Bolstad <tim@skilpaddelabs.com>
2024-09-15 16:52:03 -0400

    Added start search with keyboard return key

--- CityWeather/WeatherScreen/SearchViewController.swift
+++ CityWeather/WeatherScreen/SearchViewController.swift
@@ -34,7 +34,7 @@ class SearchView: UIView {
 
     weak var delegate: SearchViewDelegate?
 
-    let textView = UITextView()
+    let textField = UITextField()
     let placeholderLabel = UILabel()
     let searchButton = UIButton()
 
@@ -44,11 +44,14 @@ class SearchView: UIView {
 
         self.delegate = delegate
 
-        textView.layer.borderColor = UIColor.gray.cgColor
-        textView.layer.borderWidth = 1
-        textView.layer.cornerRadius = 8
-        textView.font = UIFont.systemFont(ofSize: 24)
-        textView.delegate = self
+        textField.layer.borderColor = UIColor.gray.cgColor
+        textField.layer.borderWidth = 1
+        textField.layer.cornerRadius = 8
+        textField.font = UIFont.systemFont(ofSize: 24)
+        textField.delegate = self
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 50))
+        textField.leftView = paddingView
+        textField.leftViewMode = .always
 
         placeholderLabel.textColor = UIColor.Text.placeholder
         placeholderLabel.font = UIFont.italicSystemFont(ofSize: 24)
@@ -68,7 +71,7 @@ class SearchView: UIView {
 
     @objc
     func searchButtonTapped() {
-        textView.resignFirstResponder()
+        textField.resignFirstResponder()
     }
 
     required init?(coder: NSCoder) {
@@ -77,25 +80,25 @@ class SearchView: UIView {
     
     private func setupViewLayout() {
 
-        textView.translatesAutoresizingMaskIntoConstraints = false
+        textField.translatesAutoresizingMaskIntoConstraints = false
         placeholderLabel.translatesAutoresizingMaskIntoConstraints = false
         searchButton.translatesAutoresizingMaskIntoConstraints = false
 
-        addSubview(textView)
+        addSubview(textField)
         addSubview(placeholderLabel)
         addSubview(searchButton)
 
         let constraints = [
-            textView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            textView.topAnchor.constraint(equalTo: topAnchor),
-            textView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            textField.leadingAnchor.constraint(equalTo: leadingAnchor),
+            textField.topAnchor.constraint(equalTo: topAnchor),
+            textField.bottomAnchor.constraint(equalTo: bottomAnchor),
 
-            placeholderLabel.leadingAnchor.constraint(equalTo: textView.leadingAnchor, constant: 10),
-            placeholderLabel.trailingAnchor.constraint(equalTo: textView.trailingAnchor),
-            placeholderLabel.topAnchor.constraint(equalTo: textView.topAnchor),
-            placeholderLabel.bottomAnchor.constraint(equalTo: textView.bottomAnchor),
+            placeholderLabel.leadingAnchor.constraint(equalTo: textField.leadingAnchor, constant: 10),
+            placeholderLabel.trailingAnchor.constraint(equalTo: textField.trailingAnchor),
+            placeholderLabel.topAnchor.constraint(equalTo: textField.topAnchor),
+            placeholderLabel.bottomAnchor.constraint(equalTo: textField.bottomAnchor),
 
-            searchButton.leadingAnchor.constraint(equalTo: textView.trailingAnchor, constant: 6),
+            searchButton.leadingAnchor.constraint(equalTo: textField.trailingAnchor, constant: 6),
             searchButton.trailingAnchor.constraint(equalTo: trailingAnchor),
             searchButton.topAnchor.constraint(equalTo: topAnchor),
             searchButton.bottomAnchor.constraint(equalTo: bottomAnchor),
@@ -105,24 +108,32 @@ class SearchView: UIView {
     }
 
     private func showPlaceholder(_ showPlaceholder: Bool) {
-        placeholderLabel.isHidden = showPlaceholder
-        searchButton.isEnabled = showPlaceholder
+        placeholderLabel.isHidden = !showPlaceholder
+        searchButton.isEnabled = !showPlaceholder
     }
 }
 
-extension SearchView : UITextViewDelegate {
-    func textViewDidChange(_ textView: UITextView) {
-        showPlaceholder(!textView.text.isEmpty)
-    }
+extension SearchView : UITextFieldDelegate {
 
-    func textViewDidEndEditing(_ textView: UITextView) {
-        showPlaceholder(!textView.text.isEmpty)
-        if !textView.text.isEmpty {
-            delegate?.performSearch(city: textView.text)
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+
+        guard let newText = textField.text else {
+            showPlaceholder(true)
+            return
+        }
+        showPlaceholder(newText.isEmpty)
+        if !newText.isEmpty {
+            let searchText = newText.trimmingCharacters(in: .whitespacesAndNewlines)
+            delegate?.performSearch(city: searchText)
         }
     }
 
-    func textViewDidBeginEditing(_ textView: UITextView) {
-        showPlaceholder(true)
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        showPlaceholder(false)
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
